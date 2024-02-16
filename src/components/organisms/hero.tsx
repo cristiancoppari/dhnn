0a48914993c83ab648edb654245985e8b21ead82
Cristian <cristian.coppari@gmail.com>
2024-02-16 02:42:17 -0300

    chore: remove Image component and change to picture

--- src/components/organisms/hero.tsx
+++ src/components/organisms/hero.tsx
@@ -1,20 +1,22 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 import ExpandableBox from "@/components/molecules/expandable-box";
 
 export default function Hero() {
     return (
         <section className="relative flex flex-col">
-            <div className="relative h-screen-mobile md:h-screen-desktop">
-                <Image
-                    src="/images/plane.webp"
-                    alt="Hero"
-                    fill
-                    className="object-cover"
-                    priority
-                    sizes="100vw"
-                    quality={80}
-                />
+            <div className="relative md:h-screen-desktop">
+                <picture>
+                    <source
+                        srcSet="/images/hero.webp"
+                        media="(min-width: 768px)"
+                    />
+                    <img
+                        src="/images/hero-mobile.webp"
+                        alt="Aeronex"
+                        className="w-full object-contain md:h-full md:object-cover"
+                    />
+                </picture>
             </div>
 
             <div className="relative md:absolute md:bottom-0 md:left-0">
